526a07aebda30e0c5fe9a71a3f48d39e7d839836
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-06 00:18:05 -0600

    created Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public Rigidbody2D playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
