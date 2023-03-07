a20b29f04525a6e5829a8af4364446e36eeb008c
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-06 20:23:19 -0600

    created referencePoint in Platform script

--- Assets/Scripts/PlatformCreator.cs
+++ Assets/Scripts/PlatformCreator.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlatformCreator : MonoBehaviour
 {
+    public GameObject platformPrefab;
+    public Transform referencePoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(platformPrefab, referencePoint.position, Quaternion.identity);
     }
 
     // Update is called once per frame
