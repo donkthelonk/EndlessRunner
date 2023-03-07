ca6ea85279a5d0f785920b7817f99c84c26f2cf3
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-06 21:19:09 -0600

    limited camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,25 @@ public class CameraController : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] Vector3 cameraVelocity;
     [SerializeField] float smoothTime = 1;
+    [SerializeField] bool lookAtPlayer;
+    [SerializeField] float lowerLimit = -18;
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
-        Vector3 targetPosition = new Vector3(transform.position.x, player.position.y, transform.position.z);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraVelocity, smoothTime);
+        if(player.position.y > lowerLimit)
+        {
+            //Not Smooth Movement
+            //transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+
+            //Smooth movement
+            Vector3 targetPosition = new Vector3(transform.position.x, player.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraVelocity, smoothTime);
+            // Below causes an up/down perspective effect; only works in Projection mode Perspective; tbh kinda weird
+            if (lookAtPlayer)
+            {
+                transform.LookAt(player);
+            }
+        }
     }
 }
