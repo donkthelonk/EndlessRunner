5c5b2b15786fc7b71b24615f8529bbf54fd6423a
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-06 20:27:47 -0600

    using referencePoint to spawn new platforms

--- Assets/Scripts/PlatformCreator.cs
+++ Assets/Scripts/PlatformCreator.cs
@@ -7,15 +7,21 @@ public class PlatformCreator : MonoBehaviour
     public GameObject platformPrefab;
     public Transform referencePoint;
 
+    private GameObject lastCreatedPlatform;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(platformPrefab, referencePoint.position, Quaternion.identity);
+        lastCreatedPlatform = Instantiate(platformPrefab, referencePoint.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // when platform reaches a certain point, spawn a new platform
+        if(lastCreatedPlatform.transform.position.x < 0)
+        {
+            lastCreatedPlatform = Instantiate(platformPrefab, referencePoint.position, Quaternion.identity);
+        }
     }
 }
