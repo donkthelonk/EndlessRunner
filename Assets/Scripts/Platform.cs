f0dba11d00e346d75e470d9ca9c467873957c074
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-06 20:56:45 -0600

    zoomed out camera. other changes.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     [SerializeField] int speed = 2;
+    [SerializeField] int xDestroy = 40;
 
     // Update is called once per frame
     void Update()
@@ -12,7 +13,7 @@ public class Platform : MonoBehaviour
         //Move Platform
         //transform.Translate(-1, 0, 0);
         transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
-        if(transform.position.x < -15)
+        if(transform.position.x < -xDestroy)
         {
             Destroy(gameObject);
         }
