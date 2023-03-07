c490402063efb321f53bfb671ad3c92325cc790c
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-06 21:01:45 -0600

    smoothed out camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform player;
+    [SerializeField] Vector3 cameraVelocity;
+    [SerializeField] float smoothTime = 1;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraVelocity, smoothTime);
     }
 }
