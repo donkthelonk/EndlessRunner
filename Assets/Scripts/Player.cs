957b1382b1da86b1bacf55c9dd6bb74ea434a5e9
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-07 13:32:50 -0600

    added Raycasting to detect what is beneath the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRb;
     [SerializeField] float jumpForce = 5;
+    [SerializeField] Transform raycastOrigin;
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+
+        // Find out what is beneath the player
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, Vector2.down);
+
+        // hit null check
+        if(hit.collider != null)
+        {
+            // Send name of object to console
+            Debug.Log(hit.transform.name);
+
+            // Draw the ray under the player
+            Debug.DrawRay(raycastOrigin.position, Vector2.down, Color.green);
+        }
     }
     
     void Jump()
