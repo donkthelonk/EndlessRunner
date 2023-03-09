2c8060e2e0e9eae6d8628491d3af30052802b833
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-09 10:27:03 -0600

    added Sky script and AudioSource to SkySunset

---
+++ Assets/Scripts/Sky.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sky : MonoBehaviour
+{
+    [SerializeField] AudioClip nightSound;
+    [SerializeField] AudioClip daySound;
+    [SerializeField] AudioSource audioSource;
+
+    public void PlayNightSound()
+    {
+
+    }
+
+    public void PlayDaySound()
+    {
+
+    }
+}
