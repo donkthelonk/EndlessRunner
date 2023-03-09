6a32122234351d2ea3aaba1feb3f1a747d9a5f03
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-08 21:55:02 -0600

    added camera offset. matched background color to backdrop.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
     [SerializeField] float smoothTime = 1;
     [SerializeField] bool lookAtPlayer;
     [SerializeField] float lowerLimit = -18;
+    [SerializeField] float offset = 2;
 
     // Update is called once per frame
     void Update()
@@ -19,7 +20,7 @@ public class CameraController : MonoBehaviour
             //transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
 
             //Smooth movement
-            Vector3 targetPosition = new Vector3(transform.position.x, player.position.y, transform.position.z);
+            Vector3 targetPosition = new Vector3(transform.position.x, player.position.y + offset, transform.position.z);
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraVelocity, smoothTime);
             // Below causes an up/down perspective effect; only works in Projection mode Perspective; tbh kinda weird
             if (lookAtPlayer)
