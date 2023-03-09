d8765e7abdcb2ca57abdc7d46c4945c35aed7949
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-08 23:07:50 -0600

    environment elements changes

--- Assets/Scripts/EnvironmentController.cs
+++ Assets/Scripts/EnvironmentController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnvironmentController : MonoBehaviour
 {
-    [SerializeField] GameObject environmentElement;
+    [SerializeField] GameObject[] environmentElement;
     [SerializeField] Transform referencePoint;
 
     // Start is called before the first frame update
@@ -21,8 +21,11 @@ public class EnvironmentController : MonoBehaviour
 
     IEnumerator CreateEnvironmentElement()
     {
-        Instantiate(environmentElement, referencePoint.position, Quaternion.identity);
-        yield return new WaitForSeconds(3);
+        // create offset to spawn environmet elements farther to the right
+        Vector3 offset = new Vector3(10, 2, 0);
+
+        Instantiate(environmentElement[Random.Range(0, environmentElement.Length)], referencePoint.position + offset, Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(3, 6));
         StartCoroutine(CreateEnvironmentElement());
     }
 }
