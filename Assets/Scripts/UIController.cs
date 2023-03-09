fdc5cf4a574160bc9a42cdb9663dd7a4afc8fd7e
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-09 10:52:47 -0600

    added ambient sound that changes with the sky

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -11,11 +11,18 @@ public class UIController : MonoBehaviour
     [SerializeField] GameObject gameOverScreen;
     [SerializeField] Player player;
     [SerializeField] GameObject gameMusic;
+    [SerializeField] GameObject sky;
 
     public void ShowGameOverScreen()
     {
         // sets the game over screen to active
         gameOverScreen.SetActive(true);
+
+        // turn off game music
+        gameMusic.SetActive(false);
+
+        // turn off sky and sky sound
+        sky.SetActive(false);
 
         // remove decimal places from player.distanceTraveled
         float roundedDistance = Mathf.Ceil(player.distanceTraveled);
@@ -26,8 +33,7 @@ public class UIController : MonoBehaviour
         // display number of coins collected in text field
         collectedCoins.text = "Coins Collected: " + player.GetCollectedCoins();
 
-        // turn off game music
-        gameMusic.SetActive(false);
+
     }
 
     public void GameRestart()
