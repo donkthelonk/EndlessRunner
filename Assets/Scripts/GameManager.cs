a1a426bba02943b6ecb80cb5905e227cc04b3a55
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-12 11:05:09 -0500

    added pause functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] bool paused;
+    [SerializeField] GameObject pauseScreen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,38 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
+    }
+
+    // Method to flip paused state and show/hide the Pause Screen
+    void ChangePaused()
+    {
+        // change paused state and show canvas
+        if (!paused)
+        {
+            // Flip bool
+            paused = true;
+
+            // Display pause panel
+            pauseScreen.SetActive(true);
+
+            // Stop time
+            Time.timeScale = 0;
+        }
+        // change paused state and hide canvas
+        else
+        {
+            // Flip bool
+            paused = false;
+
+            // Hide pause panel
+            pauseScreen.SetActive(false);
+
+            // Start time
+            Time.timeScale = 1;
+        }
     }
 }
