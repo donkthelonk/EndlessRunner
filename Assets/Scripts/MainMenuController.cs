ba8670bb8f4ffb36f74f3dc4c6f2d5000572fbb3
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-15 18:28:27 -0500

    code cleanup for MainMenuController script

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,18 +8,6 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] GameObject startMenu;
     [SerializeField] GameObject tutorialMenu;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // Method for the Start Button
     public void StartGame()
     {
